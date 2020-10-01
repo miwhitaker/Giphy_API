e709871c7e377656a0a7b3e05b8464c22fd1b95b
Michael Whitaker <miwhitaker@ymail.com>
2020-10-01 15:24:35 -0500

    finished javascript files

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,13 @@ var maxlength = '3';
 $('#button1').click(function() {
     var inputval = $('.text1').val();
     var fullUrl = `https://api.giphy.com/v1/gifs/search?api_key=8xgt0YKOwVYA8sYEiPaFKWlKr9p6XeLk&q=${inputval}&limit=${maxlength}&offset=0&rating=g&lang=en`;
-
+    if (inputval === '') {
+        $('.error').text('You forgot to type something in the box');
+        return;
+    }
     $.get(fullUrl).done(function(response) {
         $('.imgcontainer').empty();
+        $('.error').empty();
         var gifs = response.data
 
         for (var i = 0; i < maxlength; i = i + 1) {
