6981812a1741cc8b3ab520bb2665a4c6c104e457
Michael Whitaker <miwhitaker@ymail.com>
2020-10-04 17:02:54 -0500

    final edits to scripts and styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,14 @@
 var APIkey = '8xgt0YKOwVYA8sYEiPaFKWlKr9p6XeLk';
-var maxlength = '3';
+var maxlength = '15';
 
-$('#button1').click(function() {
+$('.button1').click(function() {
     var inputval = $('.text1').val();
     var fullUrl = `https://api.giphy.com/v1/gifs/search?api_key=8xgt0YKOwVYA8sYEiPaFKWlKr9p6XeLk&q=${inputval}&limit=${maxlength}&offset=0&rating=g&lang=en`;
     if (inputval === '') {
         $('.error').text('You forgot to type something in the box');
-        return;
+        return
     }
+
     $.get(fullUrl).done(function(response) {
         $('.imgcontainer').empty();
         $('.error').empty();
@@ -16,7 +17,6 @@ $('#button1').click(function() {
         for (var i = 0; i < maxlength; i = i + 1) {
             var newEL = `<img src="${gifs[i].images.fixed_height.url}" alt="${gifs[i].title}" />`;
             $('.imgcontainer').append(newEL);
-            console.log(gifs)
         }
     })
 });
